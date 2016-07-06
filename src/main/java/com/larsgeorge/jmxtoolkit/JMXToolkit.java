c800403eac95e290e8d93b80dcf6f76adc376df9
Lars George <larsgeorge@apache.org>
2016-07-06 09:56:03 +0200

    Updates for v2.0
    - Switched to Maven (away from ANT)
    - Added assembly descriptor
    
    Both changes are work-in-progress still.

--- src/main/java/com/larsgeorge/jmxtoolkit/JMXToolkit.java
+++ src/main/java/com/larsgeorge/jmxtoolkit/JMXToolkit.java
@@ -1,13 +1,9 @@
 /**
- * Copyright 2007 The Apache Software Foundation
+ * Copyright 2010 Lars George
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -17,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.hbase.jmxtoolkit;
+
+package com.larsgeorge.jmxtoolkit;
 
 import java.io.BufferedReader;
 import java.io.File;
